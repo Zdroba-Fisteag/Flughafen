386b6389526df04455f7af68b7e573d1069a4391
Armin-Rafael Zdroba-Fisteag <zdr240654@spengergasse.at>
2026-06-01 15:01:17 +0200

    Flughafen_Status_1

--- src/main/java/Flughafen.java
+++ src/main/java/Flughafen.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Flughafen {
+    public String ICAO_Code;
+    public List<Flugzeug> flugzeuge;
+
+    public Flughafen(String ICAO_Code, List<Flugzeug> flugzeuge) {
+        this.ICAO_Code = ICAO_Code;
+        this.flugzeuge = flugzeuge;
+    }
+
+    public Flughafen() {
+    }
+
+    public void importMitarbeiter() {
+
+    }
+
+    public void exportMitarbeiter() {
+
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Flughafen flughafen)) return false;
+
+        return ICAO_Code.equals(flughafen.ICAO_Code);
+    }
+
+    @Override
+    public int hashCode() {
+        return ICAO_Code.hashCode();
+    }
 }
